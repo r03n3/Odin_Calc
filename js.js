610c5eb798672028659cc64c3ebf83ab419ff849
r03n3 <r03n3@web.de>
2023-03-22 17:44:55 +0100

    feat: basic button functionality for operands and numbers

--- js.js
+++ js.js
@@ -4,6 +4,9 @@ function sum(num1, num2){
 }
 
 function div(num1, num2){
+    if(num2===0){
+        return "ERROR!ALARM!";
+    }
     let result=num1/num2;
     return result;
 }
@@ -18,7 +21,51 @@ function sub(num1, num2){
     return result;
 }
 
+function disp(num){
+ const dis = document.getElementById("display");
+ dis.innerHTML = num;
+}
+
 function calculator(){
+ let display=0;
+ let value=0;
+ let numcount = 0;
+ let firstOp=0;
+ let opcount=0;
+ let valueOpStore;
+ const buttonsNum = document.querySelectorAll('.number');
+ const buttonsOp =  document.querySelectorAll('.operand');
+ const equals = document.querySelectorAll('');
+ buttonsNum.forEach(buttonNum =>{
+    buttonNum.addEventListener('click', () =>{
+        if(numcount===0){
+            value = buttonNum.getAttribute('data-value');
+            numcount++;
+            display=parseInt(value);
+            disp(display);
+        }
+        else{
+            value = buttonNum.getAttribute('data-value');
+            display = display*10 + parseInt(value);
+            disp(display);
+        }
+    })
+ })
+ buttonsOp.forEach(buttonOp =>{
+    buttonOp.addEventListener('click', () =>{
+        let valueOP = buttonOp.getAttribute('data-value');
+        valueOpStore = valueOP;
+        firstOp=display;
+        numcount=0;
+        if(opcount>0){
+            display=operate(firstOp,display,operand);
+            disp(display);
+            opcount=0;
+        }      
+    })
+ })
+ button
+    disp(display);
 
 }
 
